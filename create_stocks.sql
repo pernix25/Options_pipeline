44504af6346737750b028a5e4155c88e0c6aad7b
pernix25 <mwcarroll828@gmail.com>
2026-06-29 21:00:16 -0600

    Added documentation for the stocks table creation file

--- create_stocks.sql
+++ create_stocks.sql
@@ -1,4 +1,13 @@
-/*2026-06-28 MC: Created stocks.stocks table*/
+/*2026-06-28 MC: Created stocks.stocks table
+
+
+
+stock_id: Unique identifier for stock information
+ticker: stock ticker symbol used on robinhood
+stock_nm: full name of the stock
+load_dts: time the record was uploaded into the table
+*/
+
 create table stocks.stocks(
   stock_id serial primary key
   , ticker varchar(6)
